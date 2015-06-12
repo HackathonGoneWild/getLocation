2c9a9ee989d1151781d93f2a32e5d21b97ca54d6
tomer <tomerosenfeld007@gmail.com>
2015-06-12 19:36:09 +0300

    added a function that fillters the items by distance

--- src/com/example/getLocation/MyActivity.java
+++ src/com/example/getLocation/MyActivity.java
@@ -2,19 +2,14 @@ package com.example.getLocation;
 
 
 import android.app.Activity;
-import android.location.Location;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
 import android.widget.Toast;
+import com.parse.ParseObject;
 
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
-import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
-import com.google.android.gms.location.LocationServices;
+import java.util.ArrayList;
+
 
 /**
  * Location sample.
@@ -64,5 +59,49 @@ public class MyActivity extends Activity{
 
             }
         });
+    }
+
+
+
+    private ArrayList <ParseObject> getCloseItems(ArrayList<ParseObject> objects, double lat,
+                                                  double lng, double distnace){
+        ArrayList<Double> itemsLat = new ArrayList<Double>();
+        ArrayList<Double> itemsLng = new ArrayList<Double>();
+
+        ArrayList<ParseObject> newObjects = new ArrayList<ParseObject>();
+
+        for (int i = 0; i < objects.size(); i++){
+            itemsLat.set(i, objects.get(i).getDouble("Lat"));
+            itemsLng.set(i, objects.get(i).getDouble("Lng"));
+            if(distance(itemsLat.get(i), itemsLng.get(i), lat, lng) < distnace){
+                newObjects.add(objects.get(i));
+            }
+        }
+
+        return newObjects;
+    }
+
+    private double distance(double lat1, double lon1, double lat2, double lon2) {
+
+        double theta = lon1 - lon2;
+
+        double dist = Math.sin(deg2rad(lat1)) * Math.sin(deg2rad(lat2)) + Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * Math.cos(deg2rad(theta));
+
+        dist = Math.acos(dist);
+
+        dist = rad2deg(dist);
+
+        dist = dist * 60 * 1.1515;
+
+        return (dist * 1.609344 );
+    }
+
+
+    private double rad2deg(double rad) {
+        return (rad * 180 / Math.PI);
+    }
+
+    private double deg2rad(double deg) {
+        return (deg * Math.PI / 180.0);
     }
 }
